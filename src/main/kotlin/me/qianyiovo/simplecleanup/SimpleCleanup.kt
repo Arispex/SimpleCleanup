8fcd5553976d5f3ac907635717c2f21b3226d3df
Qianyiovo <qianyiovo@outlook.com>
2023-04-15 15:56:14 +0800

    add a new notification: ActionBar

--- src/main/kotlin/me/qianyiovo/simplecleanup/SimpleCleanup.kt
+++ src/main/kotlin/me/qianyiovo/simplecleanup/SimpleCleanup.kt
@@ -1,14 +1,16 @@
 package me.qianyiovo.simplecleanup
+
+import net.md_5.bungee.api.ChatMessageType
+import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.ChatColor
+import org.bukkit.configuration.Configuration
 import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.entity.Item
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
-import net.md_5.bungee.api.ChatMessageType
-import net.md_5.bungee.api.chat.TextComponent
 
-class SimpleCleanup : JavaPlugin(){
+class SimpleCleanup : JavaPlugin() {
     lateinit var customConfig: FileConfiguration
     override fun onEnable() {
         saveDefaultConfig()
@@ -24,14 +26,29 @@ class SimpleCleanup : JavaPlugin(){
     }
 
     private fun scheduleCleanTask() {
+        val notification = customConfig.getHashMap("Notification")
+
         // 提示掉落物将在1分钟后清理
+        val message = notification["Message"].toString().toBoolean()
+        val actionBar = notification["ActionBar"].toString().toBoolean()
+
         object : BukkitRunnable() {
             override fun run() {
-                server.onlinePlayers.forEach {
-                    it.sendLocalizedMessage(
-                        "${ChatColor.WHITE}[${customConfig.getString("Prefix")}${ChatColor.WHITE}]${ChatColor.RED}一分钟后清理掉落物",
-                        "${ChatColor.WHITE}[${customConfig.getString("Prefix")}${ChatColor.WHITE}]${ChatColor.RED}The dropped items will be cleared in 1 minute"
-                    )
+                if (message) {
+                    server.onlinePlayers.forEach {
+                        it.sendLocalizedMessage(
+                            "${ChatColor.WHITE}[${customConfig.getString("Prefix")}${ChatColor.WHITE}]${ChatColor.RED}一分钟后清理掉落物",
+                            "${ChatColor.WHITE}[${customConfig.getString("Prefix")}${ChatColor.WHITE}]${ChatColor.RED}The dropped items will be cleared in 1 minute"
+                        )
+                    }
+                }
+                if (actionBar) {
+                    server.onlinePlayers.forEach {
+                        it.sendLocalizedActionBarMessage(
+                            "${ChatColor.WHITE}[${customConfig.getString("Prefix")}${ChatColor.WHITE}]${ChatColor.RED}一分钟后清理掉落物",
+                            "${ChatColor.WHITE}[${customConfig.getString("Prefix")}${ChatColor.WHITE}]${ChatColor.RED}The dropped items will be cleared in 1 minute"
+                        )
+                    }
                 }
             }
         }.runTaskLater(this@SimpleCleanup, customConfig.getLong("CleanupIntervalTickets") - 1200)
@@ -47,34 +64,65 @@ class SimpleCleanup : JavaPlugin(){
     }
 
     fun cleanup() {
+        val notification = customConfig.getHashMap("Notification")
+
+        val message = notification["Message"].toString().toBoolean()
+        val actionBar = notification["ActionBar"].toString().toBoolean()
+
         val removeItems = server.worlds
             .flatMap { it.entities }
             .filterIsInstance<Item>()
             .onEach { it.remove() }
         Global.items.addAll(removeItems)
-        server.onlinePlayers.forEach{it.sendLocalizedMessage("${ChatColor.WHITE}[${customConfig.getString("Prefix")}${ChatColor.WHITE}]${ChatColor.GREEN}已清理${removeItems.count()}个掉落物",
-           "${ChatColor.WHITE}[${customConfig.getString("Prefix")}${ChatColor.WHITE}]${ChatColor.GREEN}${removeItems.count()} dropped items have been cleared")}
+        if (message) {
+            server.onlinePlayers.forEach {
+                it.sendLocalizedMessage(
+                    "${ChatColor.WHITE}[${customConfig.getString("Prefix")}${ChatColor.WHITE}]${ChatColor.GREEN}已清理${removeItems.count()}个掉落物",
+                    "${ChatColor.WHITE}[${customConfig.getString("Prefix")}${ChatColor.WHITE}]${ChatColor.GREEN}${removeItems.count()} dropped items have been cleared"
+                )
+            }
+        }
+        if (actionBar) {
+            server.onlinePlayers.forEach{
+                it.sendLocalizedActionBarMessage(
+                    "${ChatColor.WHITE}[${customConfig.getString("Prefix")}${ChatColor.WHITE}]${ChatColor.GREEN}已清理${removeItems.count()}个掉落物",
+                    "${ChatColor.WHITE}[${customConfig.getString("Prefix")}${ChatColor.WHITE}]${ChatColor.GREEN}${removeItems.count()} dropped items have been cleared"
+                )
+            }
+        }
     }
 }
+
 fun Player.sendLocalizedMessage(zh: String, en: String) {
     if (locale.startsWith("zh")) {
         sendMessage(zh)
-    }
-    else {
+    } else {
         sendMessage(en)
     }
 }
+
 fun Player.sendLocalizedPermissionErrorMessage() {
-    sendLocalizedMessage("${ChatColor.RED}你没有权限访问此命令", "${ChatColor.RED}Yon don't have permission to access this command")
+    sendLocalizedMessage(
+        "${ChatColor.RED}你没有权限访问此命令",
+        "${ChatColor.RED}Yon don't have permission to access this command"
+    )
 }
+
 fun Player.sendLocalizedCommandNotFound() {
     sendLocalizedMessage("${ChatColor.RED}不存在此命令", "${ChatColor.RED}Command not found")
 }
+
 fun Player.sendLocalizedActionBarMessage(zhMessage: String, enMessage: String) {
     if (locale.startsWith("zh")) {
         spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent(zhMessage))
-    }
-    else {
+    } else {
         spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent(enMessage))
     }
+}
+
+fun Configuration.getHashMap(path: String): HashMap<String, Any> {
+    val hashMap = hashMapOf<String, Any>()
+    val configuration = getConfigurationSection(path)
+    configuration?.getKeys(false)?.forEach { hashMap[it] = configuration.get(it) as Any }
+    return hashMap
 }
